7966551761a7b571557f67cd62830288e2414e45
LeandroMartins0 <48505572+LeandroMartins0@users.noreply.github.com>
2020-03-18 16:44:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mvc_ctg2.Entities;
+
+namespace mvc_ctg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            menu MeuMenu = new menu(new operacoes(), new dados());
+
+            MeuMenu.MostraMenu();
+        }
+    }
+}
